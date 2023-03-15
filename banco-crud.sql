167d832c0610ec5f1111506ad737710c78316968
Adonis Oliveira <adonis.silva@jdctech.com.br>
2023-03-15 14:18:58 -0300

    NEW BANCO

--- banco-crud.sql
+++ banco-crud.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 10-Mar-2023 às 19:21
+-- Tempo de geração: 14-Mar-2023 às 23:15
 -- Versão do servidor: 10.4.27-MariaDB
--- versão do PHP: 8.0.25
+-- versão do PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -38,13 +38,15 @@ CREATE TABLE `marcas` (
 --
 
 INSERT INTO `marcas` (`id`, `nome_marca`, `data_alteracao`) VALUES
-(1, 'Marca Deletada', '2023-03-08 17:34:23'),
-(7, 'Marca Deletada', '2023-03-08 17:36:46'),
-(8, 'Marca Deletada', '2023-03-08 17:45:23'),
-(10, 'Marca Deletada', '2023-03-08 19:25:36'),
-(13, 'Marca Deletada', '2023-03-09 17:36:57'),
-(31, 'Marca1', '2023-03-10 15:15:47'),
-(32, 'Marca2', '2023-03-10 15:15:53');
+(2, 'ACERO', '2023-03-14 22:18:15'),
+(3, 'ALIPERTI', '2023-01-21 22:18:15'),
+(4, 'ATE', '2023-02-07 22:18:15'),
+(5, 'AUTHO MIX', '2022-05-16 22:18:15'),
+(6, 'AZP', '2023-06-20 22:18:15'),
+(7, 'BASTOS', '2023-07-17 22:18:15'),
+(8, 'BREMBO', '2023-09-12 22:18:15'),
+(9, 'GONEL', '2023-11-10 22:18:15'),
+(10, 'HIPPER FREIOS', '2023-03-14 22:18:15');
 
 -- --------------------------------------------------------
 
@@ -67,8 +69,36 @@ CREATE TABLE `produtos` (
 --
 
 INSERT INTO `produtos` (`id`, `nome`, `id_marca`, `quantidade`, `data_alteracao`, `preco_compra`, `preco_venda`) VALUES
-(144, 'Produto1', 31, 12, '2023-03-10 15:21:22', '12.00', '14.00'),
-(145, 'Produto2', 32, 15, '2023-03-10 15:21:19', '12.30', '13.50');
+(146, 'Tanque Reservatório Combustível VW', 2, 50, '2023-03-14 22:40:15', '450.00', '513.00'),
+(149, 'Tanque Reservatório Combustível Palio Siena Punto Idea Aço 48 Litros', 2, 30, '2023-03-14 22:40:15', '480.00', '544.00'),
+(150, 'Jogo 4 Mola Esportiva Ford Fusion 2006 a 2009 Dianteiro Traseiro', 3, 10, '2023-03-14 22:43:56', '1590.00', '1690.00'),
+(151, 'Jogo 2 Mola GNV Honda City Fit 2009 a 2018 Traseiro Aliperti AL-456G', 3, 40, '2023-03-14 22:43:56', '314.00', '334.00'),
+(152, 'Cilindro Mestre Freio Duplo Crossfox Fox Spacefox 2002 a 2014 ATE 7628', 4, 15, '2023-03-14 22:43:56', '438.00', '498.00'),
+(153, 'Amortecedor Volkswagen Fox 2010 a 2018 Traseiro SP271 Monroe SP271', 4, 90, '2023-03-14 22:43:56', '121.00', '151.00'),
+(154, 'Kit Embreagem Fiat Palio 1.0 1.3 96 a 2016 Valeo 228091', 4, 42, '2023-03-14 22:43:56', '200.00', '240.00'),
+(155, 'Filtro Ar Gol G5 G6 Fox Voyage Saveiro 1.6 8V 2006 a 2019 MANN-', 4, 88, '2023-03-14 22:43:56', '15.00', '22.00'),
+(156, 'Filtro Óleo HB20 Picanto 1.0 1.1 2011 a 2022 MANN-FILTER W6030', 4, 200, '2023-03-14 22:43:56', '22.00', '30.00'),
+(157, 'Kit Pastilha Disco Freio Toyota Corolla 2003 a 2007 Dianteiro Trw Ventilado', 4, 43, '2023-03-14 22:43:56', '319.00', '359.00'),
+(166, 'Coxim Motor Palio Siena Strada 96 a 2002 Dianteiro Passageiro Autho Mix', 5, 66, '2023-03-14 22:50:56', '500.00', '600.00'),
+(167, 'Bucha Bandeja Balança Mobi Palio Siena Uno 2005 a 2021 Dianteiro', 5, 32, '2023-03-14 22:50:56', '900.00', '1200.00'),
+(168, 'Coxim Motor Fiorino Uno 91 a 2003 Dianteiro Motorista Passageiro Autho', 5, 200, '2023-03-14 22:50:56', '260.00', '360.00'),
+(172, 'Braço Oscilante Fiat Uno 86 a 2010 Dianteiro AZP', 6, 75, '2023-03-14 22:56:23', '100.00', '138.00'),
+(173, 'Bandeja Balança 206 207 2000 a 2015 Dianteiro Motorista Com Pivô e', 6, 112, '2023-03-14 22:56:23', '150.00', '200.00'),
+(174, 'Kit Reparo Braço Oscilante Palio Weekend Tipo Brava 97 a 2020', 6, 65, '2023-03-14 22:56:23', '800.00', '900.00'),
+(175, 'Junta Cabeçote Motor Ecosport Fiesta Courier ka 1.6 99 a 2014', 7, 11, '2023-03-14 22:57:52', '200.00', '223.00'),
+(176, 'Par Selo Bloco Motor 63,3mm Fiat 500 Doblo Fiorino 2001 a 2022 Bastos', 7, 400, '2023-03-14 22:57:52', '5.00', '10.00'),
+(177, 'Junta Tampa Válvula Motor Citroen C4 Xsara Peugeot 206 1.6 2005 a', 7, 55, '2023-03-14 22:57:52', '200.00', '250.00'),
+(178, 'Jogo Junta Motor Fiorino Palio 2000 a 2017 Parcial Sem Retentor Bastos', 7, 78, '2023-03-14 22:57:52', '129.00', '149.00'),
+(179, 'Kit Embreagem Toyota Etios 1.3 1.5 2013 a 2015 Brembo 3000001340', 8, 67, '2023-03-14 22:59:11', '500.00', '527.00'),
+(180, 'Reservatório Água Limpador Parabrisa Gol G6 Trend 2013 a 2016', 9, 56, '2023-03-14 23:00:10', '100.00', '137.00'),
+(181, 'Reservatório Água Radiador Prisma 2007 a 2018 2 Saídas Gonel', 9, 67, '2023-03-14 23:00:10', '60.00', '70.00'),
+(182, 'Reservatório Água Radiador Ford Verona 90 a 92 2 Saídas Gonel', 9, 87, '2023-03-14 23:00:10', '48.00', '58.00'),
+(183, 'Reservatório Água Radiador Novo Uno 1.6 2011 a 2018 Gonel', 9, 35, '2023-03-14 23:00:10', '69.00', '79.00'),
+(184, 'Reservatório Água Radiador Peugeot 207 2009 a 2015 2 Saídas', 9, 18, '2023-03-14 23:00:10', '101.00', '141.00'),
+(185, 'Cubo Roda Uno Mille 1.0 90 a 2013 Traseiro Com Rolamento Hipper', 10, 120, '2023-03-14 23:08:49', '51.00', '71.00'),
+(186, 'Disco Freio Chevrolet Opala 81 a 92 Dianteiro Ventilado Hipper Freios', 10, 78, '2023-03-14 23:08:49', '203.00', '233.00'),
+(187, 'Par Tambor Freio Chevrolet Tracker 2020 a 2023 Traseiro Sem Cubo 5', 10, 0, '2023-03-14 23:08:49', '400.00', '422.00'),
+(188, 'Kit Pastilha Disco Freio Argo Mobi Palio Uno Grand Siena 2010 a 2021', 10, 51, '2023-03-14 23:08:49', '200.00', '240.00');
 
 -- --------------------------------------------------------
 
@@ -90,11 +120,11 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `cpf`, `nascimento`, `senha`, `tipo_usuario`) VALUES
-(1, 'Gerente1', '123.123.123-12', '2013-03-08', '202cb962ac59075b964b07152d234b70', 1),
-(33, 'Gerente2', '111.222.333-44', '2023-03-01', '202cb962ac59075b964b07152d234b70', 1),
-(34, 'Funcionario1', '111.111.111-11', '2023-02-28', '202cb962ac59075b964b07152d234b70', 2),
-(35, 'Funcionario2', '222.222.222-22', '2023-03-01', '202cb962ac59075b964b07152d234b70', 2),
-(36, 'Funcionario3', '333.333.333-33', '2023-03-03', '202cb962ac59075b964b07152d234b70', 2);
+(1, '1', '123.123.123-12', '2013-03-08', '123456789', 1),
+(33, '2', '111.222.333-44', '2023-03-01', '123456789', 1),
+(34, '3', '111.111.111-11', '2023-02-28', '123456789', 2),
+(35, '4', '222.222.222-22', '2023-03-01', '123456789', 2),
+(36, '5', '333.333.333-33', '2023-03-03', '123456789', 2);
 
 --
 -- Índices para tabelas despejadas
@@ -133,7 +163,7 @@ ALTER TABLE `marcas`
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=146;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=205;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
